1d5edc88efaf142b366738beb85410fad5ca32ce
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-05-28 14:18:11 +0200

    Cards-WAR.py

--- Cards-WAR.py
+++ Cards-WAR.py
@@ -21,6 +21,7 @@ player_2_cards = []
 player_1_score = 0
 player_2_score = 0
 round_counter = 0
+clear = "\n" * 50
 
 while len(player_1_cards) <= 9:
     player_1_cards.append(random.randint(0, 12))
@@ -34,6 +35,7 @@ print(f'{player_1} ma {len(player_1_cards)} kart {player_1_hidden_cards}')
 print(f'{player_2} ma {len(player_2_cards)} kart {player_2_hidden_cards}')
 
 input('Naciśnij Enter aby rozpocząć grę...')
+print(clear)
 
 while len(player_1_cards) > 0:
     round_counter += 1
@@ -51,6 +53,7 @@ while len(player_1_cards) > 0:
         player_2_cards.pop(0)
         print(f'{player_2} wygrywa rundę i zdobywa punkt.')
     input('Naciśnij Enter...')
+    print((clear))
 
 if player_1_score == player_2_score:
     print(f'Remis!!! Nie często się to zdarza...')
