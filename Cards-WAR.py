d9a3fe402d67c458dcfbc5f9ab4268071614dedc
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-05-28 14:13:47 +0200

    Cards-WAR.py

--- Cards-WAR.py
+++ Cards-WAR.py
@@ -14,11 +14,13 @@ player_2 = input('Podaj imię drugiego gracza -> ')
 print('Rozdaję po 10 kart dla każdego gracza...')
 
 # rozdaję po 10 kart dla każdego gracza
+# tworzę listy losowych kart dla każdego zawodnika
 
 player_1_cards = []
 player_2_cards = []
 player_1_score = 0
 player_2_score = 0
+round_counter = 0
 
 while len(player_1_cards) <= 9:
     player_1_cards.append(random.randint(0, 12))
@@ -33,14 +35,28 @@ print(f'{player_2} ma {len(player_2_cards)} kart {player_2_hidden_cards}')
 
 input('Naciśnij Enter aby rozpocząć grę...')
 
-print(f'{player_1} wykłada pierwszą kartę -> {player_1_cards[1]}')
-print(f'{player_2} wykłada pierwszą kartę -> {player_2_cards[1]}')
-
-while len(player_1_cards) < 1:
-    if player_1_cards[1] > player_2_cards[1]:
+while len(player_1_cards) > 0:
+    round_counter += 1
+    print((f'Runda {round_counter}'))
+    print(f'{player_1} wykłada kartę -> {player_1_cards[0]}')
+    print(f'{player_2} wykłada kartę -> {player_2_cards[0]}')
+    if player_1_cards[0] > player_2_cards[0]:
         player_1_score += 1
+        player_1_cards.pop(0)
+        player_2_cards.pop(0)
         print(f'{player_1} wygrywa rundę i zdobywa punkt.')
     else:
         player_2_score += 1
+        player_1_cards.pop(0)
+        player_2_cards.pop(0)
         print(f'{player_2} wygrywa rundę i zdobywa punkt.')
-input('Naciśnij Enter aby rozegrać kolejną rundę...')
+    input('Naciśnij Enter...')
+
+if player_1_score == player_2_score:
+    print(f'Remis!!! Nie często się to zdarza...')
+elif player_1_score > player_2_score:
+    print(f'Gratulacje {player_1}, zdobywasz {player_1_score} punktów i wygrywasz wojnę !!!')
+    print(f'Twój przeciwnik zdobył tylko {player_2_score} punktów.')
+else:
+    print(f'Gratulacje {player_2}, zdobywasz {player_2_score} punktów i wygrywasz wojnę !!!')
+    print(f'Twój przeciwnik zdobył tylko {player_1_score} punktów.')
